de07284b99a626b076cf382f15a3dcb3ad655245
JMan <60026263+JMantis0@users.noreply.github.com>
2020-08-25 14:51:06 -0700

    Resolved dependency issues by correcting package.json.  Also added some holy components.
    NOTE: Added several dependencies including bootstrap and react-bootstrap

--- client/src/components/TheLord.js
+++ client/src/components/TheLord.js
@@ -3,8 +3,9 @@ import Col from "react-bootstrap/Col";
 
 function TheLord(props) {
   return (
-    <Col xs={1}>
+    <Col xs={12}>
       <img
+      className="theLord hvr-grow"
         src={
           "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/baby-jesus-rebecca-mike.jpg"
         }
--- client/src/components/TheLordsComponent.js
+++ client/src/components/TheLordsComponent.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row";
 import TheLord from "./TheLord";
+import TheLordsInput from "./TheLordsInput";
+import TheLordsOutput from "./TheLordsOutput";
 
 function TheLordsComponent (props) {
+
+  let [ prayerState, setPrayerState] = useState("...")
 
   return (
     <Container>
       <h1 className="lordsTitle">Praise Him</h1>
       <Row>
         <TheLord />
+      </Row>
+      <TheLordsInput setPrayer={(prayer) => setPrayerState(prayer)}/>
+      <Row>
+        <TheLordsOutput prayerOutput={prayerState}/>
       </Row>
     </Container>
   )
