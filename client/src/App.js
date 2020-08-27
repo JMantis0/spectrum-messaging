68181a1d3a3a5dd6b13d3fd979d7664c36a1066f
JMan <60026263+JMantis0@users.noreply.github.com>
2020-08-27 13:53:06 -0700

    removed sone jsx from lord path and added config and model directories for db

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <Route path="/lord">
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+      <Route path="lord">
           <TheLordsComponent />
-        </div>
       </Route>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
