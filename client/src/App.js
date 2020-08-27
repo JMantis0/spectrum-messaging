b403863216ec50416a7d5d759c727ca1dcb81ebb
JMan <60026263+JMantis0@users.noreply.github.com>
2020-08-26 21:26:59 -0700

    reinstalled node-modules

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import TheLordsComponent from "./components/TheLordsComponent";
 
-
-import Chat from './components/Chat/Chat';
-import Join from './components/Join/Join';
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <TheLordsComponent />
-
-    </div>
     <Router>
+      <Route path="/lord">
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <TheLordsComponent />
+        </div>
+      </Route>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
     </Router>
   );
-}
+};
 
 export default App;
