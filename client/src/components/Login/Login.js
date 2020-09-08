2c761308adfabefcf85c591879a8584393c77cfc
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-08 09:35:47 -0700

    Added span to ResponsiveDrawer.js to surround the emoji (to eliminate a react warning.  Also removed unused vars and code from several files to eliminate warnings

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,6 +3,29 @@ import { Link } from "react-router-dom";
 import LoginButton from "../LoginButton/LoginButton";
 import "./Login.css";
 
+// This is the javascript for the remember me button.
+
+// const rmCheck = document.getElementById("rememberMe"),
+//     emailInput = document.getElementById("email");
+
+// if (localStorage.checkbox && localStorage.checkbox !== "") {
+//   rmCheck.setAttribute("checked", "checked");
+//   emailInput.value = localStorage.username;
+// } else {
+//   rmCheck.removeAttribute("checked");
+//   emailInput.value = "";
+// }
+
+// function lsRememberMe() {
+//   if (rmCheck.checked && emailInput.value !== "") {
+//     localStorage.username = emailInput.value;
+//     localStorage.checkbox = rmCheck.value;
+//   } else {
+//     localStorage.username = "";
+//     localStorage.checkbox = "";
+//   }
+// }
+
 function Login({ user, isAuthenticated }) {
   console.log(isAuthenticated);
   return (
