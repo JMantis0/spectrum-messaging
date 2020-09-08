420e7a6f510723d496507eabd42b37ddd0c60c5d
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-08 13:57:01 -0700

    removed curly braces from redirectUri assignment, was causing problems

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={"http://localhost:3000"}
+    redirectUri="http://localhost:3000"
     audience="https://jmantis-auth.us.auth0.com/api/v2/"
     scope="read:current_user update:current_user_metadata"
   >
