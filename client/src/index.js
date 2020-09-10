83b2c865b6290f28f1a9609f91dafa0cb5d8cc48
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-09-09 19:00:29 -0700

    discard

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-// require("dotenv").config();
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -7,8 +6,8 @@ import registerServiceWorker from "./registerServiceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-console.log(domain, clientId)
-console.log(window.location.origin)
+console.log(domain, clientId);
+console.log(window.location.origin);
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
