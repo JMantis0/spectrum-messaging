83ee3f523c17569e0c03514f091c80ffb9f65e85
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-15 19:24:16 -0700

    added a AppBar to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,69 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Chat from "./components/Chat/Chat";
+
+import {makeStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import CssBaseLine from "@material-ui/core/CssBaseline";
+
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CssBaseLine from '@material-ui/core/CssBaseline';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  }
+}));
 
 const App = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [userList, setUserList] = useState([]);
+  const classes = useStyles();
+  console.log("classes", classes);
 
   return (
-    <React.Fragment>
+    <div className={classes.root}>
       <CssBaseLine />
-    <Router>
-      <Route exact path="/">
-        <Login
-          user={user}
-          isAuthenticated={isAuthenticated}
-          isLoading={isLoading}
-        />
-      </Route>
-      <Route exact path="/chat">
-        <Chat
-          userList={userList}
-          setUserList={setUserList}
-          user={user}
-          isAuthenticated={isAuthenticated}
-          isLoading={isLoading}
-        />
-      </Route>
-    </Router>
-    </React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          Spectrum
+          </Typography>
+        </Toolbar>
+
+        <Router>
+          <Route exact path="/">
+            <Login
+              user={user}
+              isAuthenticated={isAuthenticated}
+              isLoading={isLoading}
+            />
+          </Route>
+          <Route exact path="/chat">
+            <Chat
+              userList={userList}
+              setUserList={setUserList}
+              user={user}
+              isAuthenticated={isAuthenticated}
+              isLoading={isLoading}
+            />
+          </Route>
+        </Router>
+      </AppBar>
+   </div>
   );
 };
 
